342dac6a509e9868c0f48316e81a70ca5eaa4252
Tulio Oliveira <tulio.ti@gmail.com>
2015-03-07 11:52:01 -0300

    Made first sale

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -19,7 +19,7 @@ class TransactionsController < ApplicationController
   end
 
   def pickup
-    @sale = Sale.find_by!(guid: param[:guid])
+    @sale = Sale.find_by!(guid: params[:guid])
     @book = @sale.book
   end
 end
